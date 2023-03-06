5321add17d8aaa6c79bfbb2f28a793a257bfef74
roynaldi19 <roynaldi19@gmail.com>
2023-03-06 23:04:46 +0700

    comit awal

--- app/src/main/java/com/roynaldi19/hitungumurroy/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/hitungumurroy/MainActivity.kt
@@ -2,10 +2,39 @@ package com.roynaldi19.hitungumurroy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.roynaldi19.hitungumurroy.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener {
+            val hariLahir = binding.edtHari.text.toString().toInt()
+            val bulanLahir = binding.edtBulan.text.toString().toInt()
+            val tahunLahir = binding.edtTahun.text.toString().toInt()
+
+            val hariIni: Int = Calendar.getInstance().get(Calendar.DAY_OF_YEAR)
+            val bulanIni: Int = Calendar.getInstance().get(Calendar.MONTH)
+            val tahunIni: Int = Calendar.getInstance().get(Calendar.YEAR)
+
+            val umurHari = hariIni - hariLahir
+            val umurBulan = bulanIni - bulanLahir
+            val umurTahun = tahunIni - tahunLahir
+
+            binding.tvResult.text = "Usia anda adalah $umurTahun tahun, $umurBulan bulan, $umurHari hari"
+
+            when(bulanLahir){
+                1 -> binding.tvZodiak.text = "Zodiak anda capricon"
+                2 -> binding.tvZodiak.text = "Zodiak Anda Aries"
+                3 -> binding.tvZodiak.text = "Zodiak Anda Aquarius"
+            }
+
+
+
+        }
     }
 }
